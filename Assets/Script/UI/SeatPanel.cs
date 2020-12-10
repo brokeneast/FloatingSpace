d39d002c2b55ddc36e9c23f37d6720657e767fc9
brokeneast <brokeneast@gmail.com>
2020-12-10 22:53:09 +0800

    角色設定介面

---
+++ Assets/Script/UI/SeatPanel.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeatPanel : MonoBehaviour
+{
+    [Header("Seat")]
+    [SerializeField] Text seatIndexText = null;
+    [Header("Character")]
+    [SerializeField] GameObject roleInfoSettingsWindow = null;
+    [SerializeField] GameObject characterPanel = null;
+    [SerializeField] GameObject settingPanel = null;
+    [SerializeField] Text nameText = null;
+    [SerializeField] Text tagText = null;
+
+    Canvas canvas;
+    public int seatIndex { get; private set; }
+    public Seat seat { get; private set; }
+    private SeatingPlanner planner;
+
+
+    private void Awake()
+    {
+        planner = FindObjectOfType<SeatingPlanner>();
+        canvas = FindObjectOfType<Canvas>();
+    }
+
+    /// <summary>
+    /// 初始化座位安排介面。
+    /// </summary>
+    public void Init(int seatIndex, Seat seat)
+    {
+        this.seat = seat;
+        SetSeatIndex(seatIndex);
+
+        SetCharacterInfoPanel();
+    }
+
+    /// <summary>
+    /// 設定指令索引值
+    /// </summary>
+    /// <param name="seatIndex"></param>
+    public void SetSeatIndex(int seatIndex)
+    {
+        this.seatIndex = seatIndex;
+
+        //座位索引
+        seatIndexText.text = ((char)(seatIndex + 65)).ToString();
+        gameObject.name = seatIndexText.text;
+    }
+
+    public void SetCharacter(RoleInfo character)
+    {
+        seat.character = character;
+        SetCharacterInfoPanel();
+    }
+
+    /// <summary>
+    /// 刪除席位。
+    /// </summary>
+    public void DeleteSeat()
+    {
+        planner.DeleteSeat(seatIndex);
+        Debug.Log("刪除" + gameObject.name);
+    }
+
+    /// <summary>
+    /// 在該席位上安排人員。
+    /// </summary>
+    public void AddCharacter()
+    {
+        RoleInfoSettingsWindow window = Instantiate(roleInfoSettingsWindow, canvas.transform).GetComponent<RoleInfoSettingsWindow>();
+        if(seat.character == null || string.IsNullOrEmpty(seat.character.id))
+        {
+            seat.character = new RoleInfo(seatIndex);
+        }
+        window.Init(seat.character, RoleInfoModified);
+    }
+
+    private void RoleInfoModified(RoleInfo info)
+    {
+        planner.SetCharacter(info, seatIndex);
+        SetCharacterInfoPanel();
+    }
+
+    /// <summary>
+    /// 移除該席位上已安排的人員。
+    /// </summary>
+    public void DeleteCharacter()
+    {
+        planner.SetCharacter(null, seatIndex);
+    }
+
+    /// <summary>
+    /// 該位置之角色上移。
+    /// </summary>
+    public void CharacterMoveUp()
+    {
+        if (seatIndex > 0)
+            planner.SwapCharacter(seatIndex, seatIndex - 1);
+    }
+
+    /// <summary>
+    /// 該位置之角色下移。
+    /// </summary>
+    public void CharacterMoveDown()
+    {
+        if (seatIndex < planner.seatingPlan.seats.Count - 1)
+            planner.SwapCharacter(seatIndex, seatIndex + 1);
+    }
+
+    /// <summary>
+    /// 角色資料介面。
+    /// </summary>
+    private void SetCharacterInfoPanel()
+    {
+        //人員確認
+        if (seat.character == null || string.IsNullOrEmpty(seat.character.id))
+        {
+            settingPanel.SetActive(true);
+            characterPanel.SetActive(false);
+        }
+        else
+        {
+            settingPanel.SetActive(false);
+            characterPanel.SetActive(true);
+            nameText.text = seat.character.displayName + " " + seat.character.description;
+            tagText.text = seat.character.tag;
+        }
+    }
+}
---
+++ Assets/Script/UI/SeatingPlannerView.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SeatingPlanner))]
+public class SeatingPlannerView : MonoBehaviour
+{
+    [SerializeField] SeatingPlanner seatingPlanner = null;
+    [SerializeField] GameObject seatingPanelPrefab = null;//單一座位安排介面
+    [SerializeField] GameObject seatingPanelContainer = null;//座位安排介面放置區域
+    [SerializeField] Text seatAmountText = null;
+    public List<SeatPanel> seatPanels = new List<SeatPanel>();
+
+    private void OnEnable()
+    {
+        seatingPlanner.OnSeatsRefreshed += Refresh;
+        seatingPlanner.OnSeatAdded += AddSeat;
+        seatingPlanner.OnSeatDeleted += DeleteSeat;
+        seatingPlanner.OnSeatModified += ModifySeat;
+        seatingPlanner.OnCharacterSwapped += SwapCharacter;
+    }
+
+    private void OnDisable()
+    {
+        seatingPlanner.OnSeatsRefreshed -= Refresh;
+        seatingPlanner.OnSeatAdded -= AddSeat;
+        seatingPlanner.OnSeatDeleted -= DeleteSeat;
+        seatingPlanner.OnSeatModified -= ModifySeat;
+        seatingPlanner.OnCharacterSwapped -= SwapCharacter;
+    }
+
+    /// <summary>
+    /// 刷新所有座位編輯畫面。
+    /// </summary>
+    /// <param name="seats"></param>
+    private void Refresh(List<Seat> seats)
+    {
+        for (int i = 0; i < seatPanels.Count; i++)
+        {
+            Destroy(seatPanels[i].gameObject);
+        }
+
+        seatPanels.Clear();
+
+        for (int i = 0; i < seats.Count; i++)
+        {
+            SeatPanel sp = Instantiate(seatingPanelPrefab, seatingPanelContainer.transform).GetComponent<SeatPanel>();
+            sp.gameObject.transform.SetSiblingIndex(i);
+            sp.Init(i, seats[i]);
+            seatPanels.Add(sp);
+        }
+
+        SeatAmountRefresh();
+    }
+
+    /// <summary>
+    /// 增加座位。
+    /// </summary>
+    private void AddSeat(Seat seat)
+    {
+        AddSeatPanelAndInit(seatPanels.Count, seat);
+        SeatAmountRefresh();
+    }
+
+    /// <summary>
+    /// 刪除該項。
+    /// </summary>
+    private void DeleteSeat(int index)
+    {
+        Destroy(seatPanels[index].gameObject);
+        seatPanels.RemoveAt(index);
+        RefreshSeatIndex();
+        SeatAmountRefresh();
+    }
+
+    /// <summary>
+    /// 座位內容變動。
+    /// </summary>
+    private void ModifySeat(int index, Seat s)
+    {
+        seatPanels[index].Init(index,s);
+    }
+
+    /// <summary>
+    /// 角色座位互換。
+    /// </summary>
+    private void SwapCharacter(RoleInfo a, RoleInfo b)
+    {
+        seatPanels[a.seatIndex].SetCharacter(a);
+        seatPanels[b.seatIndex].SetCharacter(b);
+    }
+
+    /// <summary>
+    /// 加入席位介面並初始化。
+    /// </summary>
+    private void AddSeatPanelAndInit(int i, Seat s)
+    {
+        SeatPanel sp = Instantiate(seatingPanelPrefab, seatingPanelContainer.transform).GetComponent<SeatPanel>();
+        sp.gameObject.transform.SetSiblingIndex(i);
+        sp.Init(i, s);
+        seatPanels.Add(sp);
+    }
+
+    /// <summary>
+    /// 刷新座位序號。
+    /// </summary>
+    private void RefreshSeatIndex()
+    {
+        for (int i = 0; i < seatPanels.Count; i++)
+        {
+            seatPanels[i].SetSeatIndex(i);
+        }
+    }
+
+    private void SeatAmountRefresh()
+    {
+        seatAmountText.text = string.Format("{0}/{1}", seatPanels.Count, SeatingPlanner.maxSeatAmount);
+    }
+}
